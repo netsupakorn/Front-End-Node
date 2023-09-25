e0a7dd098ee05c3661d2dba2ff6df9d9f1f268c1
netsupakorn10 <107359519+netsupakorn10@users.noreply.github.com>
2023-09-25 23:21:29 +0700

    save

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 const base_url = 'http://node50113-supakorn.proen.app.ruk-com.cloud';
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
